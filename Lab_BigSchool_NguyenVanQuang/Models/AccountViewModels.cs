2ee01c18f02fed39a24ec31b2973edbf5d774d20
Văn Quang Nguyễn <nguyenvanquang20001@gmail.com>
2022-06-10 14:21:06 +0700

    Update 10/06/2022

--- Lab_BigSchool_NguyenVanQuang/Models/AccountViewModels.cs
+++ Lab_BigSchool_NguyenVanQuang/Models/AccountViewModels.cs
@@ -79,6 +79,9 @@ namespace Lab_BigSchool_NguyenVanQuang.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
     }
 
     public class ResetPasswordViewModel
